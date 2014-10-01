61cc41b11d2143f0c7459c763421d283bd74d5a3
taiyi123 <boo_tai_yi@hotmail.com>
2014-10-01 14:42:22 +0800

    Removed comments from Parser

--- src/taskDo/Parser.java
+++ src/taskDo/Parser.java
@@ -130,7 +130,7 @@ public class Parser {
 			break;
 
 		case CATEGORY:
-			task.setCategory(commandParam); // Remind jack to change name
+			task.setCategory(commandParam);
 			break;
 
 		case TASK:
@@ -292,7 +292,7 @@ public class Parser {
 				isValid = false;
 				SummaryReport.setFeedBackMsg(MESSAGE_INVALID_SELECTION);
 			}
-			break; // spelling error for SummaryReportgetdisplaylist
+			break;
 
 		case DISPLAY:
 			if (isCategory(commandParam)) {
