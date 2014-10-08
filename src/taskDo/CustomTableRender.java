e3071b27d0adde957632ee5e3ddbb4239585af5e
Jack_Paing <a0112581@nus.edu.sg>
2014-10-09 00:56:30 +0800

    ''

---
+++ src/taskDo/CustomTableRender.java
@@ -0,0 +1,25 @@
+package taskDo;
+
+import java.awt.Color;
+import java.awt.Component;
+
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableCellRenderer;
+
+public class CustomTableRender extends DefaultTableCellRenderer{
+	public Component getTableCellRendererComponent(JTable table, Object value,
+		    boolean isSelected, boolean hasFocus, int row, int column) {
+
+		   Component rendererComp = super.getTableCellRendererComponent(table, value, isSelected, hasFocus,
+		     row, column);
+
+		   //Set foreground color
+		   rendererComp.setForeground(ColorBox.colorPool[24]);
+
+		   //Set background color
+		   rendererComp .setBackground(Color.BLACK);
+		   
+		   return rendererComp ;
+		  }
+
+}
