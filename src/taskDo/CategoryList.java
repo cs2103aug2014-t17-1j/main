d747ef62f951d11a402a768a7ef47090cc95a0f8
taiyi123 <boo_tai_yi@hotmail.com>
2014-10-23 14:48:48 +0800

    Uncommented isExistingCategory

--- src/taskDo/CategoryList.java
+++ src/taskDo/CategoryList.java
@@ -59,11 +59,11 @@ public class CategoryList {
 	}
 	
 	public static boolean isExistingCategory(String name) {
-		/*for(int i=0; i<categoryList.size(); i++) {
+		for(int i=0; i<categoryList.size(); i++) {
 			if(categoryList.get(i).getName().toLowerCase().equals(name.toLowerCase())) {
 				return true;
 			}
-		} */
+		} 
 		return false;
 	}
 	
