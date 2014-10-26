61b20cc791a526ee11b7a8b5e7d20c8c3fe36e49
taiyi123 <boo_tai_yi@hotmail.com>
2014-10-26 15:10:53 +0800

    Add soft shadow to content table panel

--- src/uiView/ContentTablePanel.java
+++ src/uiView/ContentTablePanel.java
@@ -1,7 +1,9 @@
 package uiView;
 
-import java.awt.Dimension;
+import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Font;
+import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
@@ -15,6 +17,7 @@ import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.KeyStroke;
 import javax.swing.ListSelectionModel;
+import javax.swing.border.Border;
 import javax.swing.border.TitledBorder;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
@@ -26,68 +29,72 @@ import taskDo.Task;
 
 import commonClasses.Constants;
 import commonClasses.SummaryReport;
+
 /*
  * @author Paing Zin Oo(Jack)
  */
-public class ContentTablePanel extends JPanel implements Observer{
-	private ArrayList<Task> taskList; 
+public class ContentTablePanel extends JPanel implements Observer {
+	private static final int PIXELS = 5;
+	private ArrayList<Task> taskList;
 	private JTable contentTable;
 	private JScrollPane jsp;
 	private int rowSelected;
 	private UiParent parent;
-	
-	
-	public ContentTablePanel(UiParent parent){
-		this.parent = parent; 
-		//setPreferredSize(new Dimension(500,400));
+
+	public ContentTablePanel(UiParent parent) {
+		this.parent = parent;
+		// setPreferredSize(new Dimension(500,400));
 		setPreferredSize(Constants.DIMENION_TABLE);
 		taskList = SummaryReport.getDisplayList();
 		removeAllComponentsFromCentrePanel();
 		setContentIntoTable();
 		setBackground(Constants.COLOR_CENTRE_PANEL_BG);
-		
-	}
 
+		Border border = BorderFactory.createEmptyBorder(PIXELS, PIXELS, PIXELS,
+				PIXELS);
+		this.setBorder(BorderFactory.createCompoundBorder(this.getBorder(),
+				border));
+		this.setLayout(new BorderLayout());
+
+	}
 
 	private void addListActionListener() {
-		contentTable.getSelectionModel().addListSelectionListener(new ListSelectionListener(){
+		contentTable.getSelectionModel().addListSelectionListener(
+				new ListSelectionListener() {
 
-			@Override
-			public void valueChanged(ListSelectionEvent select) {
-				rowSelected = contentTable.getSelectedRow();
-				parent.setRowSelected(rowSelected);
-				parent.updateDetailPanel();
-			}
-			
-		});
-		
+					@Override
+					public void valueChanged(ListSelectionEvent select) {
+						rowSelected = contentTable.getSelectedRow();
+						parent.setRowSelected(rowSelected);
+						parent.updateDetailPanel();
+					}
+
+				});
+
 	}
-	
-	public int getSelectedTableRow(){
+
+	public int getSelectedTableRow() {
 		return rowSelected;
 	}
 
-
 	private void setContentIntoTable() {
-		if(taskList.size()!=0){
-			String []columnTitle = Constants.COLUMNTITLES;
-			contentTable = new JTable(changeToTwoDArray(taskList),columnTitle){
-				public boolean isCellEditable(int row, int column){
+		if (taskList.size() != 0) {
+			String[] columnTitle = Constants.COLUMNTITLES;
+			contentTable = new JTable(changeToTwoDArray(taskList), columnTitle) {
+				public boolean isCellEditable(int row, int column) {
 					return false;
 				};
-				
+
 			};
 			setTableCellProperties(contentTable);
 			setContentTableColumnWidth(contentTable);
 			setContentTableProperties();
 			setJScrollPanePropCentrePane();
-			add(jsp);	
+			add(jsp);
 			addListActionListener();
 		}
-		
+
 	}
-	
-	
 
 	private void setContentTableProperties() {
 		setTableHeaderProperties();
@@ -99,23 +106,22 @@ public class ContentTablePanel extends JPanel implements Observer{
 		addFocusListener();
 	}
 
-
 	private void addFocusListener() {
-		contentTable.addFocusListener(new FocusListener(){
+		contentTable.addFocusListener(new FocusListener() {
 			@Override
 			public void focusGained(FocusEvent arg0) {
 				// TODO Auto-generated method stub
-				//contentTable.setRowSelectionInterval(0, 0);
+				// contentTable.setRowSelectionInterval(0, 0);
 				contentTable.changeSelection(0, 0, false, false);
 			}
+
 			@Override
 			public void focusLost(FocusEvent arg0) {
 				// TODO Auto-generated method stub
 				contentTable.clearSelection();
-			}	
+			}
 		});
 	}
-
 
 	private void setKeysPressed() {
 		tabKeyPressedAction();
@@ -124,61 +130,64 @@ public class ContentTablePanel extends JPanel implements Observer{
 		f3KeyPressedAction();
 	}
 
-
 	private void setRowAndColumnSelectionMode() {
 		contentTable.setGridColor(Constants.COLOR_TABLE_GRID);
 		contentTable.setRowHeight(Constants.TABLE_HEIGHT);
 		contentTable.setRowSelectionAllowed(true);
-		contentTable.getSelectionModel().setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		contentTable.getSelectionModel().setSelectionMode(
+				ListSelectionModel.SINGLE_SELECTION);
 		contentTable.setColumnSelectionAllowed(false);
 	}
 
-
 	private void setTableHeaderProperties() {
 		contentTable.getTableHeader().setReorderingAllowed(false);
-		contentTable.getTableHeader().setForeground(Constants.COLOR_TABLE_HEADER_TEXT);
+		contentTable.getTableHeader().setForeground(
+				Constants.COLOR_TABLE_HEADER_TEXT);
 		contentTable.getTableHeader().setResizingAllowed(false);
-		contentTable.getTableHeader().setBackground(Constants.COLOR_TABLE_HEADER_BG);
+		contentTable.getTableHeader().setBackground(
+				Constants.COLOR_TABLE_HEADER_BG);
 	}
 
-
 	private void tabKeyPressedAction() {
-		contentTable.getInputMap().put(KeyStroke.getKeyStroke(KeyEvent.VK_TAB,0), "Changed Focus");
+		contentTable.getInputMap().put(
+				KeyStroke.getKeyStroke(KeyEvent.VK_TAB, 0), "Changed Focus");
 		contentTable.getActionMap().put("Changed Focus", new AbstractAction() {
 			@Override
 			public void actionPerformed(ActionEvent actionEvent) {
 				System.out.println("TAB PRESSED IN TABLE");
-				parent.pressedTab(false);	
-			}			
+				parent.pressedTab(false);
+			}
 		});
 	}
-	
+
 	private void f3KeyPressedAction() {
-		contentTable.getInputMap().put(KeyStroke.getKeyStroke(KeyEvent.VK_F3,0), "F3");
+		contentTable.getInputMap().put(
+				KeyStroke.getKeyStroke(KeyEvent.VK_F3, 0), "F3");
 		contentTable.getActionMap().put("F3", new AbstractAction() {
 			@Override
 			public void actionPerformed(ActionEvent actionEvent) {
 				System.out.println("F3 Presed IN TABLE");
-				parent.pressedF3();	
-			}			
+				parent.pressedF3();
+			}
 		});
 	}
-	
 
 	private void f1KeyPressedAction() {
-		contentTable.getInputMap().put(KeyStroke.getKeyStroke(KeyEvent.VK_F1,0), "Event");
+		contentTable.getInputMap().put(
+				KeyStroke.getKeyStroke(KeyEvent.VK_F1, 0), "Event");
 		contentTable.getActionMap().put("Event", new AbstractAction() {
 			@Override
 			public void actionPerformed(ActionEvent actionEvent) {
 				parent.pressedF1();
-				
+
 			}
-			
+
 		});
 	}
-	
+
 	private void f2KeyPressedAction() {
-		contentTable.getInputMap().put(KeyStroke.getKeyStroke(KeyEvent.VK_F2,0), "e");
+		contentTable.getInputMap().put(
+				KeyStroke.getKeyStroke(KeyEvent.VK_F2, 0), "e");
 		System.out.println("F2 KEy is pressed");
 		contentTable.getActionMap().put("e", new AbstractAction() {
 
@@ -186,62 +195,71 @@ public class ContentTablePanel extends JPanel implements Observer{
 			public void actionPerformed(ActionEvent actionEvent) {
 				parent.pressedF2();
 				contentTable.setRowSelectionInterval(rowSelected, rowSelected);
-				
+
 			}
-			
+
 		});
 	}
 
 	private void removeAllComponentsFromCentrePanel() {
 		removeAll();
-		
+
 	}
-	private String[][] changeToTwoDArray(ArrayList<Task> taskList){
-		String tableContent[][] = new String [taskList.size()][3];
-		String dueDate ="";
-		System.out.println("CHANGE TO TWOD ARRAY "+taskList.size());
-		for (int i = 0; i < taskList.size(); i++){
-			tableContent[i][0] = (i+1)+"";
+
+	private String[][] changeToTwoDArray(ArrayList<Task> taskList) {
+		String tableContent[][] = new String[taskList.size()][3];
+		String dueDate = "";
+		System.out.println("CHANGE TO TWOD ARRAY " + taskList.size());
+		for (int i = 0; i < taskList.size(); i++) {
+			tableContent[i][0] = (i + 1) + "";
 			tableContent[i][1] = taskList.get(i).getTitle();
-			if(taskList.get(i).getDueDate().toLocalDate().getYear()==Constants.NILL_YEAR){
-				 dueDate = Constants.STRING_SOMEDAY;
-			} else{
-				DateTimeFormatter dateFormat = DateTimeFormat.forPattern("dd-MM-yyyy");
-				dueDate= dateFormat.print(taskList.get(i).getDueDate().toLocalDate());
+			if (taskList.get(i).getDueDate().toLocalDate().getYear() == Constants.NILL_YEAR) {
+				dueDate = Constants.STRING_SOMEDAY;
+			} else {
+				DateTimeFormatter dateFormat = DateTimeFormat
+						.forPattern("dd-MM-yyyy");
+				dueDate = dateFormat.print(taskList.get(i).getDueDate()
+						.toLocalDate());
 			}
 			tableContent[i][2] = dueDate;
- 		}
+		}
 		return tableContent;
 	}
-	
+
 	private void setContentTableColumnWidth(JTable contentTable) {
 		contentTable.getColumnModel().getColumn(0).setMaxWidth(20);
 		contentTable.getColumnModel().getColumn(1).setMaxWidth(600);
 		contentTable.getColumnModel().getColumn(2).setMaxWidth(100);
 	}
-	
+
 	private void setTableCellProperties(JTable contentTable) {
-		for(int i =0 ; i < taskList.size(); i++){
-			contentTable.getColumnModel().getColumn(0).setCellRenderer(new CustomTableRender());
-			contentTable.getColumnModel().getColumn(1).setCellRenderer(new CustomTableRender());
-			contentTable.getColumnModel().getColumn(2).setCellRenderer(new CustomTableRender());	
+		for (int i = 0; i < taskList.size(); i++) {
+			contentTable.getColumnModel().getColumn(0)
+					.setCellRenderer(new CustomTableRender());
+			contentTable.getColumnModel().getColumn(1)
+					.setCellRenderer(new CustomTableRender());
+			contentTable.getColumnModel().getColumn(2)
+					.setCellRenderer(new CustomTableRender());
 		}
 	}
-	
+
 	private void setJScrollPanePropCentrePane() {
 		jsp = new JScrollPane(contentTable);
-		TitledBorder jScrollTitledBorder = BorderFactory.createTitledBorder(null,Constants.HEADER_TAKSLIST, TitledBorder.DEFAULT_JUSTIFICATION, TitledBorder.DEFAULT_POSITION, Font.getFont("times new roman"), Constants.COLOR_TABLE_HEADER_TEXT);
+		TitledBorder jScrollTitledBorder = BorderFactory.createTitledBorder(
+				null, Constants.HEADER_TAKSLIST,
+				TitledBorder.DEFAULT_JUSTIFICATION,
+				TitledBorder.DEFAULT_POSITION, Font.getFont("times new roman"),
+				Constants.COLOR_TABLE_HEADER_TEXT);
 		jsp.setBorder(jScrollTitledBorder);
 		jsp.setPreferredSize(Constants.DIMESION_JSCROLL_PANEL);
 		jsp.setBackground(Constants.COLOR_JSCROLL_BG);
 		jsp.getViewport().setBackground(Constants.COLOR_JSCROLL_BG);
 	}
-	
-	public void highlightRow(){
+
+	public void highlightRow() {
 		contentTable.setRowSelectionInterval(rowSelected, rowSelected);
 		contentTable.requestFocusInWindow();
 	}
-	
 
 	@Override
 	public void update() {
@@ -250,6 +268,18 @@ public class ContentTablePanel extends JPanel implements Observer{
 		setContentIntoTable();
 		setBackground(Constants.COLOR_CENTRE_PANEL_BG);
 		parent.updateFrame();
-		
+
+	}
+
+	@Override
+	protected void paintComponent(Graphics g) {
+		int shade = 0;
+		int topOpacity = 80;
+		for (int i = 0; i < PIXELS; i++) {
+			g.setColor(new Color(shade, shade, shade,
+					((topOpacity / PIXELS) * i)));
+			g.drawRect(i, i, this.getWidth() - ((i * 2) + 1), this.getHeight()
+					- ((i * 2) + 1));
+		}
 	}
 }
